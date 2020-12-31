d0b4dae62cbfba0940f78cad1005e608dc9bb5c2
Furqan <furqan@weareavp.com>
2020-12-31 21:23:24 +0500

    Integrated created engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount DataUpdateAnalyze::Engine, at: '/data_update_analyze'
+  root to: 'users#index'
   devise_for :users, :controllers => {:registrations => "registrations"}
 
   devise_scope :user do
